3a317dd93ea06711788c5e8759e762bd1de3400e
saunakpc <saunak@zendrive.com>
2024-04-30 14:02:55 +0530

    Implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,17 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import {
+  authMiddleware,
+  clerkMiddleware,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/"]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+export default authMiddleware({
+  publicRoutes: ["/api/webhooks/clerk"],
 });
+// const isProtectedRoute = createRouteMatcher(["/"]);
+
+// export default clerkMiddleware((auth, req) => {
+//   if (isProtectedRoute(req)) auth().protect();
+// });
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
